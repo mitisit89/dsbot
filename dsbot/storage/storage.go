a1eb955bc5fec49d1c9a2243d58c2fc2a3cbfec3
mitisit <29955395+mitisit89@users.noreply.github.com>
2024-12-02 12:42:10 +0300

    refactor

--- dsbot/storage/storage.go
+++ dsbot/storage/storage.go
@@ -5,8 +5,9 @@ import "context"
 type Movie struct {
 	Names []string
 }
+
 type Storage interface {
-	Add(ctx context.Context, m *Movie) error
-	MarkedAsWatched(ctx context.Context, m *Movie) error
+	Add(ctx context.Context, m string) error
+	MarkedAsWatched(ctx context.Context, m string) error
 	GetAll(ctx context.Context) (*Movie, error)
 }
