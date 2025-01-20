fe392c7a09403fda78251978bda3f31bdfaf9577
mitisit <29955395+mitisit89@users.noreply.github.com>
2025-01-20 21:38:08 +0300

    refactor:migration to pg

--- schema.sql
+++ schema.sql
@@ -1,5 +1,30 @@
-create table if not exists watchlist (
-id integer primary key autoincrement,
-name string not null unique,
-watched integer default 0
+
+create table if not exists user(
+    id serial primary key,
+    name varchar(255) not null,
+
+)
+create table if not exists movie (
+    id serial primary key ,
+    name varchar(255) not null,
+    watched bool default false,
 );
+create table if not exists game(
+    id serial primary key,
+    name varchar(255) not null
+)
+
+create table if not exists user_movie_rating(
+    id serial primary key ,
+    rating smallint check(rating > 0 and rating <= 10),
+    foreign key(user_id) references user(id) on delete cascade,
+    foreign key(movie_id) references movie(id) on delete cascade
+)
+
+create table if not exists games_rating(
+    id serial primary key,
+    name varchar(255) not null,
+    rating smallint check(rating > 0 and rating <= 10),
+    foreign key(user_id) references user(id) on delete cascade
+    foreign key(game_id) references game(id) on delete cascade
+)
