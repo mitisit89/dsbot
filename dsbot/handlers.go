ef14d302c025b7c1a8d83dd12475ee568907fe29
mitisit <29955395+mitisit89@users.noreply.github.com>
2025-01-26 01:31:15 +0300

    feat:add new cmds

--- dsbot/handlers.go
+++ dsbot/handlers.go
@@ -92,22 +92,42 @@ var CommandHandlers = map[string]func(s *discordgo.Session, i *discordgo.Interac
 		})
 
 	},
-	"test-embed": func(s *discordgo.Session, i *discordgo.InteractionCreate) {
+	"game-list": func(s *discordgo.Session, i *discordgo.InteractionCreate) {
+
+		c := storage.New()
+		gameList, err := c.GameList(context.Background())
+		if err != nil {
+			slog.Error("failed to get game list ", err)
+		}
 		s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
 			// Ignore type for now, they will be discussed in "responses"
 			Type: discordgo.InteractionResponseChannelMessageWithSource,
 			Data: &discordgo.InteractionResponseData{
-				Embeds: []*discordgo.MessageEmbed{
-					{
-						Title: "Test Embed",
-						Fields: []*discordgo.MessageEmbedField{
-							{
-								Name:   "Test Field",
-								Inline: true,
-							},
-						},
-					},
-				},
+				Embeds: []*discordgo.MessageEmbed{GenerateEmbed(gameList.Names)},
+			},
+		})
+	},
+	"add-to-game-list": func(s *discordgo.Session, i *discordgo.InteractionCreate) {
+		var msgformat string
+		opts := i.ApplicationCommandData().Options
+		optsMap := make(map[string]*discordgo.ApplicationCommandInteractionDataOption, len(opts))
+		for _, opt := range opts {
+			optsMap[opt.Name] = opt
+		}
+
+		if option, ok := optsMap["game"]; ok {
+			c := storage.New()
+			if err := c.AddGame(context.TODO(), i.Member.User.Username, option.StringValue()); err != nil {
+				slog.Error("failed to add game to list", err)
+			}
+			msgformat = fmt.Sprintf("> You add to game list: %s\n", option.StringValue())
+		}
+
+		s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+
+			Type: discordgo.InteractionResponseChannelMessageWithSource,
+			Data: &discordgo.InteractionResponseData{
+				Content: msgformat,
 			},
 		})
 	},
