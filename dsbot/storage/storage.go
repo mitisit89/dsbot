7c2ba63d0ca001ecd91b100b22c1503711daa08d
mitisit <29955395+mitisit89@users.noreply.github.com>
2025-01-28 00:33:14 +0300

    refactor:add new interface

--- dsbot/storage/storage.go
+++ dsbot/storage/storage.go
@@ -1,6 +1,11 @@
 package storage
 
-import "context"
+import (
+	"context"
+
+	"github.com/jackc/pgx"
+	"github.com/jackc/pgx/v5/pgconn"
+)
 
 type Movie struct {
 	Names []string
@@ -13,4 +18,24 @@ type Storage interface {
 	Add(ctx context.Context, m string) error
 	MarkedAsWatched(ctx context.Context, m string) error
 	GetAll(ctx context.Context) (*Movie, error)
+}
+
+type DBTX interface {
+	Exec(context.Context, string, ...interface{}) (pgconn.CommandTag, error)
+	Query(context.Context, string, ...interface{}) (pgx.Rows, error)
+	QueryRow(context.Context, string, ...interface{}) pgx.Row
+}
+
+func New(db DBTX) *Queries {
+	return &Queries{db: db}
+}
+
+type Queries struct {
+	db DBTX
+}
+
+func (q *Queries) WithTx(tx pgx.Tx) *Queries {
+	return &Queries{
+		db: tx,
+	}
 }
