a7ec706f393870061982ccf03b72bdcea7592dd8
mitisit <29955395+mitisit89@users.noreply.github.com>
2025-02-03 03:39:57 +0300

    feat:add user name and trailer to embed message

--- internal/utils.go
+++ internal/utils.go
@@ -11,9 +11,16 @@ import (
 func GenerateEmbed(movies []storage.Movie, title string) *discordgo.MessageEmbed {
 	fields := make([]*discordgo.MessageEmbedField, len(movies))
 	for i, item := range movies {
-		fields[i] = &discordgo.MessageEmbedField{
-			Value:  fmt.Sprintf("* **%s**", item.Name),
-			Inline: false,
+		if item.DiscordUser.Valid && item.Trailer.Valid {
+			fields[i] = &discordgo.MessageEmbedField{
+				Value:  fmt.Sprintf("**%s**\n> [Trailer](%s)\n> Ordered by %s", item.Name, item.Trailer.String, item.DiscordUser.String),
+				Inline: false,
+			}
+		} else {
+			fields[i] = &discordgo.MessageEmbedField{
+				Value:  fmt.Sprintf("**%s**\n", item.Name),
+				Inline: false,
+			}
 		}
 	}
 
