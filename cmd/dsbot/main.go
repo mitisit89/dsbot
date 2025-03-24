a819a6b44952c4b34f24035f4195257f3cb6db68
mitisit <29955395+mitisit89@users.noreply.github.com>
2025-03-24 16:13:44 +0300

    refactor:move all session staff to separate file

--- cmd/dsbot/main.go
+++ cmd/dsbot/main.go
@@ -2,9 +2,7 @@ package main
 
 import (
 	dsbot "dsbot/internal"
-	"dsbot/internal/activities"
 	"flag"
-	"log/slog"
 	"os"
 	"os/signal"
 
@@ -14,71 +12,31 @@ import (
 
 // Bot parameters
 var (
-	GuildID        = flag.String("guild", "", "Test guild ID. If not passed - bot registers commands globally")
-	RemoveCommands = flag.Bool("rmcmd", true, "Remove all commands after shutdowning or not")
-
-	dsSession *discordgo.Session
-
+	RemoveCommands                 = flag.Bool("rmcmd", true, "Remove all commands after shutdowning or not")
 	integerOptionMinValue          = 1.0
 	dmPermission                   = false
 	defaultMemberPermissions int64 = discordgo.PermissionViewChannel
 )
 
-func init() {
-	logger := dsbot.SetUpLogger()
-	var err error
-	dsSession, err = discordgo.New("Bot " + os.Getenv("DISCORD_TOKEN"))
-	if err != nil {
-		logger.Error("Invalid bot parameters: %v", err)
-	}
-	dsSession.Identify.Intents = discordgo.IntentsGuildPresences
-	dsSession.State.MaxMessageCount = 0 // disable message cache
-	dsSession.AddHandler(func(s *discordgo.Session, i *discordgo.InteractionCreate) {
-		if h, ok := dsbot.CommandHandlers[i.ApplicationCommandData().Name]; ok {
-			go h(s, i)
-		}
-	})
-
-	dsSession.AddHandler(activities.OnStream)
-}
-
 func main() {
-	logger := dsbot.SetUpLogger()
-	dsSession.AddHandler(func(s *discordgo.Session, r *discordgo.Ready) {
-		logger.Info("Logged in as: %s#%v", s.State.User.Username, s.State.User.Discriminator)
-	})
-	err := dsSession.Open()
+	bot, err := dsbot.New()
 	if err != nil {
-		logger.Error("Cannot open the session: %v", err)
-		return
+		panic(err)
 	}
-
-	logger.Info("Adding commands...")
-	registeredCommands := make([]*discordgo.ApplicationCommand, len(dsbot.Commands))
-	for i, v := range dsbot.Commands {
-		cmd, err := dsSession.ApplicationCommandCreate(dsSession.State.User.ID, *GuildID, v)
-		if err != nil {
-			slog.Error("Cannot create '%v' command: %v", v.Name, err)
-		}
-		registeredCommands[i] = cmd
+	err = bot.Start()
+	if err != nil {
+		panic(err)
 	}
-
-	defer dsSession.Close()
-
+	defer bot.Stop()
+	addedCommands := bot.AddCommands()
 	stop := make(chan os.Signal, 1)
 	signal.Notify(stop, os.Interrupt)
-	logger.Info("Press Ctrl+C to exit")
+	bot.Logger.Info("Press Ctrl+C to exit")
 	<-stop
 
 	if *RemoveCommands {
-		logger.Info("Removing commands...")
-		for _, v := range registeredCommands {
-			err := dsSession.ApplicationCommandDelete(dsSession.State.User.ID, *GuildID, v.ID)
-			if err != nil {
-				logger.Error("Cannot delete '%v' command: %v", v.Name, err)
-			}
-		}
+		bot.RemoveCommands(addedCommands)
 	}
+	bot.Logger.Info("Gracefully shutting down.")
 
-	logger.Info("Gracefully shutting down.")
 }
--- internal/session.go
+++ internal/session.go
@@ -1,1 +1,80 @@
 package internal
+
+import (
+	"flag"
+	"log/slog"
+	"os"
+
+	"github.com/bwmarrin/discordgo"
+)
+
+var GuildID = flag.String("guild", "", "Test guild ID. If not passed - bot registers commands globally")
+
+type Session struct {
+	Session *discordgo.Session
+	Logger  *slog.Logger
+}
+
+func New() (*Session, error) {
+	logger := SetUpLogger()
+
+	s, err := discordgo.New("Bot " + os.Getenv("DISCORD_TOKEN"))
+	if err != nil {
+		logger.Error("Invalid bot parameters: %v", err)
+	}
+	s.Identify.Intents = discordgo.IntentsGuildPresences
+	bot := &Session{
+		Session: s,
+		Logger:  logger,
+	}
+	bot.registerHandlers()
+	bot.Session.AddHandler(func(s *discordgo.Session, r *discordgo.Ready) {
+		bot.Logger.Info("Logged in as", "username", s.State.User.Username, "discriminator", s.State.User.Discriminator)
+	})
+	return bot, nil
+
+}
+
+func (s *Session) registerHandlers() {
+	s.Session.AddHandler(func(s *discordgo.Session, i *discordgo.InteractionCreate) {
+		if h, ok := CommandHandlers[i.ApplicationCommandData().Name]; ok {
+			go h(s, i)
+		}
+	})
+}
+func (s *Session) Start() error {
+	if err := s.Session.Open(); err != nil {
+		s.Logger.Error("Cannot open the session", "error", err)
+		return err
+	}
+	s.Logger.Info("Bot session started")
+	return nil
+}
+
+func (s *Session) Stop() {
+	s.Logger.Info("Shutting down bot session...")
+	s.Session.Close()
+}
+func (s *Session) AddCommands() []*discordgo.ApplicationCommand {
+	s.Logger.Info("Adding commands...")
+	registeredCommands := make([]*discordgo.ApplicationCommand, len(Commands))
+	for i, v := range Commands {
+		cmd, err := s.Session.ApplicationCommandCreate(s.Session.State.User.ID, *GuildID, v)
+		if err != nil {
+			s.Logger.Error("Cannot create command", "name", v.Name, "error", err)
+			continue
+		}
+		registeredCommands[i] = cmd
+	}
+	return registeredCommands
+}
+
+func (s *Session) RemoveCommands(commands []*discordgo.ApplicationCommand) {
+	s.Logger.Info("Removing commands...")
+	for _, v := range commands {
+		err := s.Session.ApplicationCommandDelete(s.Session.State.User.ID, *GuildID, v.ID)
+		if err != nil {
+			s.Logger.Error("Cannot delete command", "name", v.Name, "error", err)
+		}
+	}
+}
