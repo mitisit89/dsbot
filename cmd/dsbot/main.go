bab795491eb6d933f8bd44b8a0bc2b284d37f626
mitisit <29955395+mitisit89@users.noreply.github.com>
2025-03-26 03:43:20 +0300

    feat:update syscalls

--- cmd/dsbot/main.go
+++ cmd/dsbot/main.go
@@ -5,6 +5,7 @@ import (
 	"flag"
 	"os"
 	"os/signal"
+	"syscall"
 
 	"github.com/bwmarrin/discordgo"
 	_ "github.com/joho/godotenv/autoload"
@@ -30,13 +31,11 @@ func main() {
 	defer bot.Stop()
 	addedCommands := bot.AddCommands()
 	stop := make(chan os.Signal, 1)
-	signal.Notify(stop, os.Interrupt)
+	signal.Notify(stop, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
 	bot.Logger.Info("Press Ctrl+C to exit")
 	<-stop
-
 	if *RemoveCommands {
 		bot.RemoveCommands(addedCommands)
 	}
 	bot.Logger.Info("Gracefully shutting down.")
-
 }
