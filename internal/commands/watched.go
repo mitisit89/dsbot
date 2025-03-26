2ad38e9472da038852775487c2750e4aadb5afc4
mitisit <29955395+mitisit89@users.noreply.github.com>
2025-03-26 03:43:01 +0300

    refactor:rm comments

--- internal/commands/watched.go
+++ internal/commands/watched.go
@@ -16,16 +16,12 @@ func Watched(s *discordgo.Session, i *discordgo.InteractionCreate) {
 	for _, opt := range opts {
 		optsMap[opt.Name] = opt
 	}
-	// margs := make([]interface{}, 0, len(opts))
 	if option, ok := optsMap["movie"]; ok {
 		c := storage.New()
 
 		if err := c.MarkAsWatched(context.TODO(), option.StringValue()); err != nil {
 			slog.Error("failed to mark as watched", err)
 		}
-		// Option values must be type asserted from interface{}.
-		// Discordgo provides utility functions to make this simple.
-		// margs = append(margs, option.StringValue())
 		msgformat = fmt.Sprintf("> You mark as watched: %s\n", option.StringValue())
 
 		err := s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
